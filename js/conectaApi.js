f9b902df8a657ffb859e1f495ea26c0db4c73e59
feliipecardosoo <fglibino@gmail.com>
2024-01-25 20:25:52 -0300

    teste

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,10 +1,35 @@
 async function listaVideos() {
     const conexao = await fetch('http://localhost:3000/videos')
     const conexaoConvertida = await conexao.json()
-    
+    return conexaoConvertida
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizacoes`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json()
     return conexaoConvertida
 }
 
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo,
+    buscaVideo
 }
